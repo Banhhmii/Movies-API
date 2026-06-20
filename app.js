7496cf5b28feb6e418b211c17ff75f0c1d072937
Banh <tommyngo156@gmail.com>
2026-06-20 00:18:52 -0400

    fix: Add app.listen for production

--- app.js
+++ app.js
@@ -518,6 +518,9 @@ app.post("/login", rateLimiter, validateLogin, (req, res, next) => {
 
 app.use(errorHandler);
 
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
+});
 
 
 module.exports = {
